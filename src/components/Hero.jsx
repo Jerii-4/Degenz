01406e6f406d956a4c4a688b2b8e62ed83eb3091
Jerii-4 <jerinbtc17@gmail.com>
2025-04-08 23:43:01 +0530

    some updates

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,11 +1,19 @@
 import React from "react";
+import search from "../assets/search.png"
 
 const HeroSection = () => {
   return (
-    <div className="min-h-screen bg-black flex items-center justify-center">
-      <h1 className="text-white text-4xl font-bold">Tailwind is not working 🎉</h1>
-    </div>
-  );
-};  
-export default HeroSection;
+    <div className="min-h-screen flex flex-col items-center justify-center bg-black-500">
+     <div className="bg-blue-500 " > <h1 className="uppercase w-8 text-white text-8xl font-bold">You have reached your <br></br> destination</h1>
+     </div>
+     <div>
+      <img src={search} className="w-16 m-8 cursor-pointer rounded-full hover:scale-120 hover:rotate-2 hover:brightness-120 transition-transform duration-300"/>
+     </div>
   
+    </div>
+
+   
+
+  ) 
+};
+export default HeroSection;
