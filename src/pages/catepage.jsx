860f5f227652326d964dbf22c99f8ce1cf6d1f4b
Jerii-4 <jerinbtc17@gmail.com>
2025-04-09 19:01:31 +0530

    around second page done

--- src/pages/catepage.jsx
+++ src/pages/catepage.jsx
@@ -3,9 +3,9 @@ import React from "react";
 import { useNavigate } from "react-router-dom";
 import back_img from "../assets/backs.png"
 
-const CatePage = () => {
-  const navigate = useNavigate();
-  const handleBack = () => navigate("/");
+    const CatePage = () => {
+    const navigate = useNavigate();
+    const handleBack = () => navigate("/");
   return (
     <div className="min-h-screen relative bg-gray-400">
       <img src={back_img}
@@ -13,9 +13,9 @@ const CatePage = () => {
         className="w-2 h-2 fixed hover:scale-120 hover:rotate-2  transition-transform duration-300 fixed top-2 left-2 cursor-pointer"
       />
       <div className="p-16 flex items-center justify-center min-h-screen">
-        <h1 className="text-8xl font-extrabold text-gray-900 tracking-wide text-center">
-          Categories 
-        </h1>
+        
+         <button type="submit" className="text-white extrabold uppercase w-12 h-16 bg-black-500 hover:scale-120 transform-translate duration-200">Categories</button>  
+        
       </div>
     </div>
   );
